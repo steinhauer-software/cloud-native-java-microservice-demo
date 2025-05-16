e566a7d2be1286da34380ac475b1ec589db8d38e
Holger Steinhauer <holger.steinhauer@steinhauer.software>
2025-05-16 22:49:23 +0200

    Initial commit

---
+++ Product Service/src/test/java/software/steinhauer/schulung/cloudnative/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package software.steinhauer.schulung.cloudnative.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
